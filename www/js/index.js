807e500e636c505ca9d2bca8b45fd173c77296d3
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-10-01 13:11:41 -0600

    fixing commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,31 +1,109 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// var cron = require('cron').CronJob;
+// var Big = require('big.js')
+// var fs = require('fs')
 
-import axios from 'axios';
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+const element = (name) => document.getElementById(name)
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    console.log('test')
-    // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    // document.getElementById('deviceready').classList.add('ready');
+var cache = {}
+
+// const dewPoint = (temperature, relative_humidity) => {
+//     var magnus = {
+//         a: 17.625,
+//         b: 243.04,
+//     }
+//     /// Math.log(relative_humidity/100) + ((magnus.a * temperature) / (magnus.b + temperature))
+//     const aFunction = () => {
+//         var r_hum = new Big(relative_humidity).div(100).toNumber()
+//         var lh = new Big(Math.log(r_hum))
+//         var numer = new Big(magnus.a).mul(temperature)
+//         var denom = new Big(magnus.b).plus(temperature)
+//         var rh = numer.div(denom)
+//         return lh.plus(rh).round(2).toNumber()
+//     }
+
+//     var left_hand = new Big(magnus.b).mul(aFunction())
+//     var right_hand = new Big(magnus.a).minus(aFunction())
+//     var result = left_hand.div(right_hand).round(2).toNumber()
+//     return result
+// }
+
+const generateCurrentWeather = async() => {
+    var { open_weather_api: { meta, data: forecast } } = await retrieveForecast()
+    if (meta.status === 200 && meta.statusText === 'OK' && meta.ok === true) {
+        var { main, weather , wind, timezone, visibility, clouds, coord, sys } = forecast
+        const setCurrentWeather = ({ 
+                temp, description, 
+                icon, wind, w_id,
+                maxtemp, mintemp, dew_point, 
+                humidity, utc_offset }) => {
+            element("temp").innerHTML = `${Math.round(temp)}&deg;C`
+            element("description").innerHTML = description
+            // element("dew_point").innerHTML = `${dew_point}&deg;C`
+            element("weather").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@4x.png" class="image-fluid" alt="${w_id}"/>`
+            element("mintemp").innerHTML = `${mintemp}&deg;C`
+            element("maxtemp").innerHTML = `${maxtemp}&deg;C`
+            element("wind").innerHTML = `${wind.speed}km/h ${wind.deg}&deg;`
+            element("humidity").innerHTML = `${humidity}RH%`
+            element("time").innerHTML = moment().utcOffset(utc_offset/60).format('hh:mmA')
+        }
+        var date = {
+            sunrise: moment.unix(sys.sunrise).format('hh:mmA'),
+            sunset: moment.unix(sys.sunset).format('hh:mmA')
+        }
+        console.log("🚀 ~ file: index.js ~ line 29 ~ job ~ date", date)
+        const weather_info = {
+            temp: main.temp,
+            description: weather?.at(0).description, 
+            icon: weather?.at(0).icon, 
+            w_id: weather?.at(0).id, 
+            wind: wind,
+            dew_point: main.dew_point,
+            maxtemp: main.temp_max, 
+            mintemp: main.temp_min, 
+            humidity: main.humidity,
+            utc_offset: timezone,
+        }
+        setCurrentWeather(weather_info)
+    }
+}
+
+window.onload = async() => {
+    await generateCurrentWeather()
+    var job = new cron('* * * * * *', async() => {
+        await generateCurrentWeather()
+    }, null, true)
+    job.start()
+    console.log("all info received from server")
+}
+
+const retrieveForecast = async() => {
+    var APIkey = "712e884a43143ab7711a408902440f8f" //insert your API key from open weather map https://home.openweathermap.org/users/sign_up
+    var geoLocation = "lethbridge,ca"
+    var url = `https://api.openweathermap.org/data/2.5/weather?q${'='}${geoLocation}&units=metric&APPID=${APIkey}`
+    var weatherURL = `https://weatherdbi.herokuapp.com/data/weather/${geoLocation}`
+    var response = await fetch(url, {
+        method: 'GET',
+    })
+    // var weatherResponse = await fetch(weatherURL, {
+    //     method: 'GET',
+    // })
+    var data = await response.json().then((item) => ({
+        ...item,
+        main: {
+            ...item?.main ?? {},
+            // dew_point: dewPoint(item.main.temp, item.main.humidity),
+        }
+    }))
+    // var weatherData = await weatherResponse.json()
+    return {
+        open_weather_api: {
+            meta: response,
+            data,
+        },
+        // weatherdbi: {
+        //     meta: weatherResponse,
+        //     data: weatherData,
+        // }
+    }
 }
